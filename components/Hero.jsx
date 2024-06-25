eede0b9ac0713873b172a0491b6e4f9855ffd5ca
Rajat-Uchiha <rajatraguvanshi3900@gmail.com>
2024-06-25 18:30:40 +0530

    added new login/signup designs

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Link from "next/link";
 const Hero = () => {
   return (
     <header className="bg-teal-600  text-white py-16">
@@ -8,12 +8,12 @@ const Hero = () => {
         <p className="text-xl mb-8">
           Engaging and enlightening talks of experts as well as of beginners
         </p>
-        <a
-          href="#features"
+        <Link
+          href="/user/signup"
           className="bg-white text-cyan-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-black hover:text-white duration-200"
         >
           Get Started
-        </a>
+        </Link>
       </div>
     </header>
   );
